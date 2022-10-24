8fd17971002af78de80109c020ac6580a28fd4f5
walacenascimento <wlcens@hotmail.com>
2022-10-24 14:26:32 -0300

    Implementa o arquivo queue, resolve o requisito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,26 @@
 class Queue:
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        self.data = []
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
+        self.data.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
+        return self.data.pop(0)
 
     def search(self, index):
         """Aqui irá sua implementação"""
+        if type(index) is not int:
+            raise IndexError
+        elif not 0 <= index <= self.__len__() - 1:
+            raise IndexError
+
+        else:
+            return self.data[index]
