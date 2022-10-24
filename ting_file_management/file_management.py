5ea1c6f70f57b143a84f7bfd0fee05aa45767ad5
walacenascimento <wlcens@hotmail.com>
2022-10-24 14:59:53 -0300

    Implementando a função txt_importer o arquivo file_management, resolve o requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file) as file:
+            text = file.read()
+            text_list = text.split("\n")
+
+        return text_list
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
