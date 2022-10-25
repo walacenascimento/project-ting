48eee2fa1fc73078bf1762271fe30b9806e99a89
walacenascimento <wlcens@hotmail.com>
2022-10-24 21:24:10 -0300

    Implementando a função exists_word, resolve o requisito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    occurrences = []
+    result = []
+    for index in range(instance.__len__()):
+        file_sentences = instance.search(index)["linhas_do_arquivo"]
+        for index_sentence in range(len(file_sentences)):
+            if word.lower() in file_sentences[index_sentence].lower():
+                occurrences.append({"linha": index_sentence + 1})
+        if len(occurrences) > 0:
+            partial_result = {
+              "palavra": word,
+              "arquivo": instance.search(index)["nome_do_arquivo"],
+              "ocorrencias": occurrences,
+            }
+
+            result.append(partial_result)
+
+    return result
 
 
 def search_by_word(word, instance):
